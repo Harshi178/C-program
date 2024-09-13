43005a7957a6442ff680543fc7edee59dcc9ea31
Harshi178 <hh2457440@gmail.com>
2024-09-13 09:26:54 +0530

    Add files via upload

---
+++ main (6).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int add(int a,int b);
+int main() {
+  int num1,num2,sum;
+  printf("enter two numbers: ");
+  scanf("%d %d", &num1,&num2);
+
+  sum=add(num1,num2);
+
+  printf("Sum=%d\n",sum);
+}
+int add(int a,int b){
+  return a % b;
+  
+}
